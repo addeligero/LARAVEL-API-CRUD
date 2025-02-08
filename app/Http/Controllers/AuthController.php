a1a8acd2d15789c98fbbd0b08fb97906750b5cdd
ADRIAN <Gwapodeligero@gmail.com>
2025-02-08 13:49:57 +0800

    feat: added validators for registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,28 +5,44 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User; // Make sure to import
 use Illuminate\Support\Facades\Hash; // Make sure to import
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $validated=$request->validate([
+        // $validated=$request->validate([
+        // 'name'=>'required|max:255|string',
+        // 'email'=>'required|email|string|max:255|unique:users',
+        // 'password'=>'required|min:6|string|confirmed'
+
+        // ]);
+
+        $validated=Validator::make($request->all(), [
         'name'=>'required|max:255|string',
         'email'=>'required|email|string|max:255|unique:users',
         'password'=>'required|min:6|string|confirmed'
 
         ]);
-        $user= User::create([
-        'name'=>$validated['name'],
-        'email'=>$validated['email'],
-        'password'=>Hash::make($validated['password']),
+
+        if ($validated->fails()) {
+            return response()->json($validated->errors(), 422);
+        }
+        try {
+            $user= User::create([
+        'name'=>$request->name,
+        'email'=>$request->email,
+        'password'=>Hash::make($request->password),
         ]);
 
-        $token=$user->createToken('auth_token')->plainTextToken;
+            $token=$user->createToken('auth_token')->plainTextToken;
 
-        return response()->json([
-            'access_token'=> $token,
-            'user'=> $user
-        ], 200);
+            return response()->json([
+                'access_token'=> $token,
+                'user'=> $user
+            ], 200);
+        } catch (\Exception $th) {
+            return response()->json(['error'=>$th->getMessage()]);
+        }
     }
 }
